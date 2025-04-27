17e7cfc47ff732d493663583e211a4612e5b0c85
lunarpotato <chantal.hann@gmail.com>
2025-04-27 18:06:11 +0200

    refactored und node package.lock.json als dic ausgeben

--- src/finder.py
+++ src/finder.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 def FindFile(pfad):
@@ -5,9 +6,31 @@ def FindFile(pfad):
         dateiname = datei.name 
 
         if dateiname == 'testpackage.json':
-            print(dateiname) 
         
             with datei.open() as f:
                 dateiInhalt = f.read()
-                print(dateiInhalt)
-                return dateiInhalt
+                packageJson = json.loads(dateiInhalt)
+    return packageJson
+            
+
+
+
+
+def readNodePackage(pfad):
+    """Lies die package.lock.json im node_modules
+        Returns: dic: package im json Format    
+    """
+    for datei in pfad.iterdir():
+            dateiname = datei.name 
+
+            if dateiname == 'node_modules':
+                modulePfad = pfad / 'node_modules'
+
+                for modulesDatei in modulePfad.iterdir():
+                     dateiname = modulesDatei.name
+
+                     if dateiname == '.package-lock.json':     
+                        with modulesDatei.open() as f:
+                            jsoncontent = f.read()
+                            nodeLockJson = json.loads(jsoncontent)
+    return nodeLockJson
--- src/main.py
+++ src/main.py
@@ -1,49 +1,22 @@
 from pathlib import Path
-from finder import FindFile
+from finder import FindFile, readNodePackage
 import json
 
 aktuellerPfad = Path(__file__).resolve().parent
 
 pfad = aktuellerPfad.parent / 'test'   
 
+packageJsonObjects = FindFile(pfad)
+
+
+dependencyPropsDic = packageJsonObjects['dependencies']
+
+devDependencyPropsDic = packageJsonObjects['devDependencies']
+
+for key, value in dependencyPropsDic.items():
+
+    print(key, value)
 
 
 
-inhalt = FindFile(pfad)
-
-
-jsonObjects = json.loads(inhalt)
-print(jsonObjects)
-
-
-dependencyPropsDic = jsonObjects['dependencies']
-
-devDependencyPropsDic = jsonObjects['devDependencies']
-
-for item, value in dependencyPropsDic.items():
-
-    print(item, value)
-
-
-
-for datei in pfad.iterdir():
-        dateiname = datei.name 
-
-        if dateiname == 'node_modules':
-            modulePfad = pfad / 'node_modules'
-
-            for inhalt in modulePfad.iterdir():
-                 dateiname = inhalt.name
-
-                 if dateiname == '.package-lock.json':     
-                    with inhalt.open() as f:
-                        dateiinhalt = f.read()
-                        print(dateiinhalt)
-                   
-
-            
-   
-
-                
-
-            
+nodeLockJson = readNodePackage(pfad)
