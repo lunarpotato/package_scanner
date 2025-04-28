451775777de942813ef32337110c5c21eab0b1a7
lunarpotato <chantal.hann@gmail.com>
2025-04-28 22:47:43 +0200

    liefert die version der testpackage.json

--- src/main.py
+++ src/main.py
@@ -14,24 +14,36 @@ dependencyPropsDic = packageJsonObjects['dependencies']
 
 devDependencyPropsDic = packageJsonObjects['devDependencies']
 
-for key, value in dependencyPropsDic.items():
-
-    print(key, value)
-
-
-
 
 nodePackageProps = nodeLockJson['packages']
 
 missedProps = list()
+# existingProps = list()
 
 strNodeModules = 'node_modules/'
 for key in dependencyPropsDic:
  
     prop = nodePackageProps.get(strNodeModules+key)
-    #Ist das Property vorhanden?
+    #Ist das Package vorhanden?
     if prop == None:
-        print('Datei nicht gefunden')
+        print(key,'wurde nicht gefunden')
         missedProps.append(key)
     else:
-        print('Datei vorhanden')
+        print(key,'vorhanden')
+        # existingProps.append(key)
+        nodeVersion = nodePackageProps.get(strNodeModules+key)
+        nodeVersionValue = nodeVersion['version']
+        print('Version',nodeVersionValue)
+
+        packageJsonValue = dependencyPropsDic[key]
+        print(packageJsonValue)
+
+
+# print(existingProps)
+
+# testVar = strNodeModules+existingProps+'version'
+# print(testVar)
+
+# version = nodePackageProps.get(strNodeModules+existingProps+['version'])
+# print(version)
+
