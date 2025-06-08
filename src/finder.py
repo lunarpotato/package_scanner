79235edf05622ee9e447ed8f5c0af1db238be765
lunarpotato <chantal.hann@gmail.com>
2025-06-08 16:08:36 +0200

    exception für package.json data handling eingebaut

--- src/finder.py
+++ src/finder.py
@@ -1,16 +1,30 @@
 import json
 from pathlib import Path
+import sys
 
 def FindFile(pfad):
-    for datei in pfad.iterdir():
-        dateiname = datei.name 
+    try:
+        for datei in pfad.iterdir():
+            dateiname = datei.name 
 
-        if dateiname == 'package.json':
+            if dateiname == 'package.json':
+            
+                with datei.open() as f:
+                    dateiInhalt = f.read()
+                    packageJson = json.loads(dateiInhalt)
+                
         
-            with datei.open() as f:
-                dateiInhalt = f.read()
-                packageJson = json.loads(dateiInhalt)
-    return packageJson
+                return packageJson
+            
+        raise Exception()
+    
+    except: 
+        print('package.json not found.')
+        input('press any key to exit.')
+        sys.exit()
+
+    # else: print ('package.json file not found. try to move this application into your projects root folder')
+
             
 def readNodePackage(pfad):
     """Lies die package.lock.json im node_modules
